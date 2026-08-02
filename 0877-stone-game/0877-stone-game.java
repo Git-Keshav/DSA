1b34a3b0cbeafba96bd6cf5ee6f86cb800b783e1
KeshavGitter <keshav.showhow@gmail.com>
2026-08-02 22:57:09 +0530

    Time: 5 ms (23.31%), Space: 43.1 MB (48.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int[] mScDiff = new int[n];
+
+        for(int i = 0; i < n; i++){
+            mScDiff[i] = piles[i];
+        }
+
+        for(int len = 2; len <= n; len++){
+            for(int i=0; i <= n - len; i++){
+                int j = i + len -1;
+
+                mScDiff[i] = Math.max(piles[i] - mScDiff[ i + 1], piles[j] - mScDiff[i]);
+            }
+        }
+        return mScDiff[0] > 0;
     }
 }
