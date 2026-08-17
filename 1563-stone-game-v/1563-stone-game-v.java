f2b99c50dcb2cf46d352dc5b8eeef757a65a1fcf
KeshavGitter <keshav.showhow@gmail.com>
2026-08-18 01:30:31 +0530

    Time: 237 ms (79.57%), Space: 47.8 MB (49.52%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,8 +1,8 @@
 class Solution {
-    int[][] memo;
+    int[][] df;
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
-        memo = new int[n][n];
+        df = new int[n][n];
         int[] prefixSum = new int[n + 1];
         for (int i = 0; i < n; i++) prefixSum[i + 1] = prefixSum[i] + stoneValue[i];
         return dfs(stoneValue, 0, n - 1, prefixSum);
@@ -10,7 +10,7 @@ class Solution {
     
     private int dfs(int[] stoneValue, int left, int right, int[] prefixSum) {
         if (left == right) return 0;
-        if (memo[left][right] != 0) return memo[left][right];
+        if (df[left][right] != 0) return df[left][right];
         int ans = 0;
         for (int i = left; i < right; i++) {
             int leftSum = prefixSum[i + 1] - prefixSum[left];
@@ -19,6 +19,6 @@ class Solution {
             else if (leftSum > rightSum) ans = Math.max(ans, rightSum + dfs(stoneValue, i + 1, right, prefixSum));
             else ans = Math.max(ans, leftSum + Math.max(dfs(stoneValue, left, i, prefixSum), dfs(stoneValue, i + 1, right, prefixSum)));
         }
-        return memo[left][right] = ans;
+        return df[left][right] = ans;
     }
 }
