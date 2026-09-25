cd0b4ef70db318f6e9b0346d2bc174a8be0bdbe9
Keshav <keshav.showhow@gmail.com>
2026-09-25 23:27:08 +0530

    Time: 8 ms (92.8%), Space: 46.7 MB (90.4%) - LeetHub

---
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<String> braceExpansionII(String expression) {
+            return dfs(expression, 0, expression.length() - 1);
+    }
+
+    private List<String> dfs(String expression, int s, int e) {
+        TreeSet<String> ans = new TreeSet<>();
+        List<List<String>> groups = new ArrayList<>();
+        groups.add(new ArrayList<>());
+        
+        int layer = 0;
+        int left = 0;
+        
+        for (int i = s; i <= e; ++i) {
+            char c = expression.charAt(i);
+            if (c == '{') {
+                if (layer == 0) left = i + 1;
+                layer++;
+            } else if (c == '}') {
+                layer--;
+                if (layer == 0) {
+
+                    List<String> innerList = dfs(expression, left, i - 1);
+                    merge(groups, innerList);
+                }
+            } else if (c == ',' && layer == 0) {
+
+                groups.add(new ArrayList<>());
+            } else if (layer == 0) {
+
+                merge(groups, new ArrayList<>(List.of(String.valueOf(c))));
+            }
+        }
+
+        for (List<String> group : groups) {
+            for (String word : group) {
+                ans.add(word);
+            }
+        }
+        
+        return new ArrayList<>(ans);
+    }
+
+    private void merge(List<List<String>> groups, List<String> group) {
+        List<String> lastGroup = groups.get(groups.size() - 1);
+        if (lastGroup.isEmpty()) {
+            groups.set(groups.size() - 1, group);
+            return;
+        }
+
+        List<String> mergedGroup = new ArrayList<>();
+        for (String word1 : lastGroup) {
+            for (String word2 : group) {
+                mergedGroup.add(word1 + word2);
+            }
+        }
+        groups.set(groups.size() - 1, mergedGroup);
+    }
+        
+    
+}
